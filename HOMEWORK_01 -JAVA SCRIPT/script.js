334256d07928181141a9b45488ec962c13197629
Hristina Bozhinova <117430805+bozhinova-hristina@users.noreply.github.com>
2022-12-07 17:40:04 +0100

    Add files via upload

---
+++ HOMEWORK_01 -JAVA SCRIPT/script.js
@@ -0,0 +1,6 @@
+
+var phonePrice = 119.95;
+var phoneTax = 0.05;
+var totalOnePhonePrice = phonePrice + (phonePrice * phoneTax);
+var totalPrice = totalOnePhonePrice *30;
+console.log(`Total phone price is ${totalPrice}$`);
