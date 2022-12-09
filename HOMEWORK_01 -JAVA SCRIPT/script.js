97200c2e44e6dad57f9548b4fcf729b1256fde85
Hristina Bozhinova <117430805+bozhinova-hristina@users.noreply.github.com>
2022-12-09 23:12:08 +0100

    Update script.js

--- HOMEWORK_01 -JAVA SCRIPT/script.js
+++ HOMEWORK_01 -JAVA SCRIPT/script.js
@@ -1,6 +1,7 @@
-
-var phonePrice = 119.95;
-var phoneTax = 0.05;
-var totalOnePhonePrice = phonePrice + (phonePrice * phoneTax);
-var totalPrice = totalOnePhonePrice *30;
+var totalPhones = 30;
+var taxRate = (5 / 100);
+var onePhonePrice = 119.95;
+var taxPrice = onePhonePrice * taxRate;
+var totalOnePhonePrice = onePhonePrice + taxPrice;
+var totalPrice = totalOnePhonePrice * totalPhones;
 console.log(`Total phone price is ${totalPrice}$`);
